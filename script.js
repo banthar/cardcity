6311f3d0477f3469c84a9d0f23015d0a5df0c567
Piotr Praszmo <ppraszmo@gmail.com>
2015-08-02 21:34:16 +0200

    scale

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 
 function main() {
   var board=document.getElementById("board");
-  board.onmousemove = function(e) {
+  var screen=document.getElementById("screen");
+  var transform = mat2d.create();
+
+  var updateTransform = function() {
+    board.style.transform = "matrix("+Array.prototype.join.call(transform, ",")+")";
+  };
+
+  screen.onmousemove = function(e) {
     if(e.buttons === 1) {
-      window.scrollBy(-e.movementX, -e.movementY);
+      var translation = mat2d.create();
+      mat2d.fromTranslation(translation, vec2.fromValues(e.movementX, e.movementY));
+      mat2d.multiply(transform, translation, transform);
+      updateTransform();
     }
     e.preventDefault();
   }
-  board.onmousedown = function(e) {
+  window.onmousedown = function(e) {
     e.preventDefault();
   }
-  board.onmousewheel = function(e) {
-    console.log(e);
+  window.onmousewheel = function(e) {
+    var scaleDelta = Math.pow(1.5, e.wheelDelta/120);
+    mat2d.scale(transform, transform, vec2.fromValues(scaleDelta, scaleDelta));
+    updateTransform();
     e.preventDefault();
   }
 }
