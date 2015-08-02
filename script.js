692548133cb469062690abd4be7ef23b6228c932
Piotr Praszmo <ppraszmo@gmail.com>
2015-08-02 23:16:15 +0200

    different colors

--- script.js
+++ script.js
@@ -1,22 +1,57 @@
 
+var cardTypes = [
+  {
+    name: "House",
+    description: "Small house",
+    color: vec3.fromValues(255.0, 0.0, 0.0),
+  },
+  {
+    name: "School",
+    description: "Small school",
+    color: vec3.fromValues(0.0, 255.0, 0.0),
+  },
+  {
+    name: "Shop",
+    description: "Small shop",
+    color: vec3.fromValues(0.0, 0.0, 255.0),
+  },
+  {
+    name: "Park",
+    description: "Small park",
+    color: vec3.fromValues(255.0, 0.0, 255.0),
+  },
+];
+
 function createCard() {
+  var type = cardTypes[Math.floor(Math.random()*cardTypes.length)];
+
   var card = document.createElement("DIV");
   card.classList.add("card")
 
   var header = document.createElement("DIV");
   header.classList.add("card-header");
-  header.innerText = "House";
+  header.innerText = type.name;
   card.appendChild(header);
   
   var description = document.createElement("DIV");
   description.classList.add("card-description");
-  description.innerText = "Small house";
+  description.innerText = type.description;
   card.appendChild(description);
 
   var footer = document.createElement("DIV");
   footer.classList.add("card-footer");
   footer.innerText = "+HH";
   card.appendChild(footer);
+
+
+  var background = type.color;
+  var lightBackground = vec3.create();
+  var limit = 200.0;
+  vec3.max(lightBackground, background, vec3.fromValues(limit, limit, limit));
+  
+  header.style.background = cssColor(background);
+  description.style.background = cssColor(lightBackground);
+  footer.style.background = cssColor(background);
 
   return card;
 }
@@ -29,8 +64,18 @@ function addCard(board, card, x, y) {
   board.appendChild(card);
 }
 
+
+function addHandCard(hand, card) {
+  hand.appendChild(card);
+}
+
+function cssColor(rgb) {
+  return "rgb("+Array.prototype.join.call(rgb, ",")+")";
+}
+
 function main() {
   var board=document.getElementById("board");
+  var hand=document.getElementById("hand");
   var screen=document.getElementById("screen");
   var transform = mat2d.create();
 
@@ -63,4 +108,9 @@ function main() {
       addCard(board, createCard(), x, y);
     }
   }
+
+  for(var y=0;y<10;y++) {
+    addHandCard(hand, createCard());
+  }
+
 }
