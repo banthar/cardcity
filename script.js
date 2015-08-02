9627962348c637427c25af7eebc108d956419c11
root <root@hirek.local>
2015-08-02 22:53:12 +0200

    hand

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
 
+function createCard() {
+  var card = document.createElement("DIV");
+  card.classList.add("card")
+
+  var header = document.createElement("DIV");
+  header.classList.add("card-header");
+  header.innerText = "House";
+  card.appendChild(header);
+  
+  var description = document.createElement("DIV");
+  description.classList.add("card-description");
+  description.innerText = "Small house";
+  card.appendChild(description);
+
+  var footer = document.createElement("DIV");
+  footer.classList.add("card-footer");
+  footer.innerText = "+HH";
+  card.appendChild(footer);
+
+  return card;
+}
+
+function addCard(board, card, x, y) {
+  card.style.position = "absolute";
+  card.style.top = y * 340;
+  card.style.left = x * 244;
+  card.style.zIndex = x+y;
+  board.appendChild(card);
+}
+
 function main() {
   var board=document.getElementById("board");
   var screen=document.getElementById("screen");
@@ -25,5 +55,12 @@ function main() {
     mat2d.scale(transform, transform, vec2.fromValues(scaleDelta, scaleDelta));
     updateTransform();
     e.preventDefault();
+  }
+  updateTransform();
+
+  for(var y=0;y<20;y++) {
+    for(var x=0;x<20;x++) {
+      addCard(board, createCard(), x, y);
+    }
   }
 }
