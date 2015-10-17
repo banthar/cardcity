77decd4434464aab0a6790236308f464507a9f5b
Piotr Praszmo <ppraszmo@gmail.com>
2015-10-17 11:15:40 +0200

    autoformat

--- script.js
+++ script.js
@@ -3,26 +3,26 @@
 var ID = 0;
 
 var cardTypes = [
-  {
+{
     name: "House",
     description: "Small house",
     color: vec3.fromValues(255.0, 0.0, 0.0),
-  },
-  {
+}, 
+{
     name: "School",
     description: "Small school",
     color: vec3.fromValues(0.0, 255.0, 0.0),
-  },
-  {
+}, 
+{
     name: "Shop",
     description: "Small shop",
     color: vec3.fromValues(0.0, 0.0, 255.0),
-  },
-  {
+}, 
+{
     name: "Park",
     description: "Small park",
     color: vec3.fromValues(255.0, 0.0, 255.0),
-  },
+}, 
 ];
 
 var cardSize = vec2.fromValues(244, 340);
@@ -30,130 +30,131 @@ var cardSize = vec2.fromValues(244, 340);
 let trace = console.log.bind(console);
 
 function createCard() {
-  var type = cardTypes[Math.floor(Math.random()*cardTypes.length)];
-
-  var card = document.createElement("DIV");
-  card.classList.add("card")
-
-  var header = document.createElement("DIV");
-  header.classList.add("card-header");
-  header.innerText = type.name;
-  card.appendChild(header);
-  
-  var description = document.createElement("DIV");
-  description.classList.add("card-description");
-  description.innerText = type.description;
-  card.appendChild(description);
-
-  var footer = document.createElement("DIV");
-  footer.classList.add("card-footer");
-  footer.innerText = "+HH";
-  card.appendChild(footer);
-
-
-  var background = type.color;
-  var lightBackground = vec3.create();
-  var limit = 200.0;
-  vec3.max(lightBackground, background, vec3.fromValues(limit, limit, limit));
-  
-  header.style.background = cssColor(background);
-  description.style.background = cssColor(lightBackground);
-  footer.style.background = cssColor(background);
-
-  return card;
+    var type = cardTypes[Math.floor(Math.random() * cardTypes.length)];
+    
+    var card = document.createElement("DIV");
+    card.classList.add("card")
+    
+    var header = document.createElement("DIV");
+    header.classList.add("card-header");
+    header.innerText = type.name;
+    card.appendChild(header);
+    
+    var description = document.createElement("DIV");
+    description.classList.add("card-description");
+    description.innerText = type.description;
+    card.appendChild(description);
+    
+    var footer = document.createElement("DIV");
+    footer.classList.add("card-footer");
+    footer.innerText = "+HH";
+    card.appendChild(footer);
+    
+    
+    var background = type.color;
+    var lightBackground = vec3.create();
+    var limit = 200.0;
+    vec3.max(lightBackground, background, vec3.fromValues(limit, limit, limit));
+    
+    header.style.background = cssColor(background);
+    description.style.background = cssColor(lightBackground);
+    footer.style.background = cssColor(background);
+    
+    return card;
 }
 
 function addCard(board, card, x, y) {
-  trace(x,y);
-  card.style.position = "absolute";
-  card.style.left = x * cardSize[0];
-  card.style.top = y * cardSize[1];
-  card.style.zIndex = x+y;
-  board.appendChild(card);
+    trace(x, y);
+    card.style.position = "absolute";
+    card.style.left = x * cardSize[0];
+    card.style.top = y * cardSize[1];
+    card.style.zIndex = x + y;
+    board.appendChild(card);
 }
 
 function addHandCard(screen, hand, card) {
-  hand.appendChild(card);
-  card.draggable = true;
-  card.id = "handCard"+(ID++);
-  card.ondragstart = function(e) {
-    e.dataTransfer.setData("card", card.id);
-  }
+    hand.appendChild(card);
+    card.draggable = true;
+    card.id = "handCard" + (ID++);
+    card.ondragstart = function(e) {
+        e.dataTransfer.setData("card", card.id);
+    }
 }
 
 function cssColor(rgb) {
-  return "rgb("+Array.prototype.join.call(rgb, ",")+")";
+    return "rgb(" + Array.prototype.join.call(rgb, ",") + ")";
 }
 
 function createDropShadow() {
-  var shadow = document.createElement("DIV");
-  shadow.width = cardSize[0];
-  shadow.height = cardSize[1];
-  return shadow;
+    var shadow = document.createElement("DIV");
+    shadow.width = cardSize[0];
+    shadow.height = cardSize[1];
+    return shadow;
 }
 
 function main() {
-  var board=document.getElementById("board");
-  var hand=document.getElementById("hand");
-  var screen=document.getElementById("screen");
-  var transform = mat2d.create();
-
-  var dropShadow = createDropShadow();
-  board.appendChild(dropShadow);
-
-  var updateTransform = function() {
-    board.style.transformOrigin = "left top";
-    board.style.transform = "matrix("+Array.prototype.join.call(transform, ",")+")";
-  };
-
-  function getBoardPosition(x, y) {
-    let invertedTransform = mat2d.create();
-    mat2d.invert(invertedTransform, transform);
-    let position = vec2.fromValues(x, y);
-    vec2.transformMat2d(position, position, invertedTransform);
-    return position;
-  } 
-
-  screen.ondrop = function(e) {
-    let card = document.getElementById(e.dataTransfer.getData("card"));
-    let position = getBoardPosition(e.x, e.y);
-    vec2.divide(position, position, cardSize);
-    addCard(board, card, Math.floor(position[0]), Math.floor(position[1]));
-  }
-
-  screen.ondragover = function(e) {
-    let position = getBoardPosition(e.x, e.y);
-    trace([e.x, e.y], position);
-    e.preventDefault();
-  }
-
-  screen.onmousedown = function(e) {
-    screen.onmousemove = function(e) {
-      if(e.buttons === 1) {
-        var translation = mat2d.create();
-        mat2d.fromTranslation(translation, vec2.fromValues(e.movementX, e.movementY));
-        mat2d.multiply(transform, translation, transform);
+    var board = document.getElementById("board");
+    var hand = document.getElementById("hand");
+    var screen = document.getElementById("screen");
+    var transform = mat2d.create();
+    
+    var dropShadow = createDropShadow();
+    board.appendChild(dropShadow);
+    
+    var updateTransform = function() {
+        board.style.transformOrigin = "left top";
+        board.style.transform = "matrix(" + Array.prototype.join.call(transform, ",") + ")";
+    }
+    ;
+    
+    function getBoardPosition(x, y) {
+        let invertedTransform = mat2d.create();
+        mat2d.invert(invertedTransform, transform);
+        let position = vec2.fromValues(x, y);
+        vec2.transformMat2d(position, position, invertedTransform);
+        return position;
+    }
+    
+    screen.ondrop = function(e) {
+        let card = document.getElementById(e.dataTransfer.getData("card"));
+        let position = getBoardPosition(e.x, e.y);
+        vec2.divide(position, position, cardSize);
+        addCard(board, card, Math.floor(position[0]), Math.floor(position[1]));
+    }
+    
+    screen.ondragover = function(e) {
+        let position = getBoardPosition(e.x, e.y);
+        trace([e.x, e.y], position);
+        e.preventDefault();
+    }
+    
+    screen.onmousedown = function(e) {
+        screen.onmousemove = function(e) {
+            if (e.buttons === 1) {
+                var translation = mat2d.create();
+                mat2d.fromTranslation(translation, vec2.fromValues(e.movementX, e.movementY));
+                mat2d.multiply(transform, translation, transform);
+                updateTransform();
+            }
+            e.preventDefault();
+        }
+        e.preventDefault();
+    }
+    screen.onmousewheel = function(e) {
+        var scaleDelta = Math.pow(1.5, e.wheelDelta / 120);
+        mat2d.scale(transform, transform, vec2.fromValues(scaleDelta, scaleDelta));
         updateTransform();
-      }
-      e.preventDefault();
+        e.preventDefault();
     }
-    e.preventDefault();
-  }
-  screen.onmousewheel = function(e) {
-    var scaleDelta = Math.pow(1.5, e.wheelDelta/120);
-    mat2d.scale(transform, transform, vec2.fromValues(scaleDelta, scaleDelta));
     updateTransform();
-    e.preventDefault();
-  }
-  updateTransform();
-
-  for(var y=0;y<2;y++) {
-    for(var x=0;x<2;x++) {
-      addCard(board, createCard(), x, y);
+    
+    for (var y = 0; y < 2; y++) {
+        for (var x = 0; x < 2; x++) {
+            addCard(board, createCard(), x, y);
+        }
     }
-  }
-
-  for(var y=0;y<10;y++) {
-    addHandCard(screen, hand, createCard());
-  }
+    
+    for (var y = 0; y < 10; y++) {
+        addHandCard(screen, hand, createCard());
+    }
 }
